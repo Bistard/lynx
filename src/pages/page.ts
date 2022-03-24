656ca0f5c5cddd8651a28c5fa80b7012b98aee98
Bistard <lshh1015813038@gmail.com>
2022-03-24 19:20:33 -0400

    init [including login interface]

---
+++ src/pages/page.ts
@@ -0,0 +1,34 @@
+import { IAccessor } from "src/workbench";
+
+let uuid = 0;
+
+export const PAGE_MANAGER = new Map<string, Page>();
+
+export const enum EPages {
+    Login1 = 'login1',
+    Login2 = 'login2',
+}
+
+export abstract class Page {
+
+    public readonly name: string;
+    public readonly id: number = uuid++;
+
+    public element: HTMLElement;
+
+    protected accessor: IAccessor;
+
+    constructor(name: string, accessor: IAccessor) {
+        this.name = name;
+        this.accessor = accessor;
+        this.element = document.createElement('div');
+        this.element.className = 'page';
+        
+        PAGE_MANAGER.set(name, this);
+        this.create();
+    }
+
+    protected abstract create(): void;
+
+    public abstract dispose(): void;
+}
---
+++ src/workbench.ts
@@ -0,0 +1,56 @@
+import { LoginPage1, LoginPage2 } from "./pages/login/login";
+import { EPages, PAGE_MANAGER } from "./pages/page";
+
+export interface IAccessor {
+
+    displayPage(name: string): boolean;
+
+}
+
+class Workbench implements IAccessor {
+    
+    public currentPage: string = 'none';
+
+    private element: HTMLElement;
+    
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'workbench';
+        
+        // creates all the pages
+        this.initPages();
+        
+        document.body.appendChild(this.element);
+    }
+
+    public initPages(): void {
+        const ctors = [
+            LoginPage1, 
+            LoginPage2,
+        ];
+        ctors.forEach(ctor => new ctor(this));
+        this.displayPage(EPages.Login1);
+    }
+
+    public displayPage(name: string): boolean {
+        if (name === this.currentPage) {
+            return false;
+        }
+
+        const page = PAGE_MANAGER.get(name);
+        if (page === undefined) {
+            return false;
+        }
+
+        if (this.element.firstChild) {
+            this.element.removeChild(this.element.firstChild);
+        }
+        this.element.appendChild(page.element);
+        this.currentPage = page.name;
+        return true;
+    }
+
+}
+
+
+new Workbench();
